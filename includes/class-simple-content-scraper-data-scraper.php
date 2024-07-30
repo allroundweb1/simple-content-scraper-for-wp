172ac065b08ca05fcba5808c0861253edfc0310c
RickOnGitHub <rickvanoirschot@gmail.com>
2024-07-30 10:40:38 +0200

    v1.0.2 fix: Keep importing while image URLs are invalid...

--- includes/class-simple-content-scraper-data-scraper.php
+++ includes/class-simple-content-scraper-data-scraper.php
@@ -507,7 +507,9 @@ class Simple_Content_Scraper_Data_Scraper
 
         if (is_wp_error($file_array['tmp_name'])) {
             // Trow an error
-            throw new Exception('Error: (simco_upload_image) ' . $file_array['tmp_name']->get_error_message());
+            // throw new Exception('Error: (simco_upload_image) ' . $file_array['tmp_name']->get_error_message());
+            @unlink($file_array['tmp_name']);
+            return '';
         }
 
         $attachment_id = media_handle_sideload($file_array, $post_id, $desc);
@@ -516,7 +518,8 @@ class Simple_Content_Scraper_Data_Scraper
             @unlink($file_array['tmp_name']);
 
             // Trow an error
-            throw new Exception('Error: (simco_upload_image) ' . $attachment_id->get_error_message());
+            // throw new Exception('Error: (simco_upload_image) ' . $attachment_id->get_error_message());
+            return '';
         }
 
         if (wp_attachment_is_image($attachment_id)) {
